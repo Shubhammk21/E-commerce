266039f3af3a510e33ce5c1f4c46e89e178e6e0f
Shubham Singh <shubhammk21@gmail.com>
2023-06-19 23:44:18 +0530

    <>

--- FrontEnd/JsFiles/ManageAddress.js
+++ FrontEnd/JsFiles/ManageAddress.js
@@ -2,15 +2,16 @@ let addresslist_append=document.querySelector("#AddressList");
 
 let customer= JSON.parse(localStorage.getItem("token"))|| [];
 
-//let addressListData= JSON.parse(localStorage.getItem("addressListData")) || [];
+let addressListData= JSON.parse(localStorage.getItem("addressListData")) || [];
 
 async function addressList(){
-    if(token==null){
+    if(customer==null){
         window.location.href="/FrontEnd\signUp.html";
     }else{
         try {
-            let res= await fetch ("http://localhost:8088/GetAddressList/"+customer.userId);// this api call the all the address by customer id;
+            let res= await fetch ('http://localhost:8088/GetAddressList/'+customer.userId);// this api call the all the address by customer id;
             let data= await res.json();
+            //console.log(data);
             if(data!=null){
                 //localStorage.removeItem("addressListData");
                 localStorage.setItem("addressListData",JSON.stringify(data));
@@ -22,15 +23,14 @@ async function addressList(){
     }
 
 }
-console.log(customer.userId);
+
 async function postAddress(data){
-    if(token==null){
+    if(customer==null){
         window.location.href="/FrontEnd\signUp.html";
     }
     else{
-        try {
-            
-            let res= await fetch(` http://localhost:8080/Add/Address/${customer.customerId}`,{// this api post the address
+        try {        
+            let res= await fetch('http://localhost:8080/Add/Address/'+customer.customerId,{// this api post the address
                 method:'POST',
                 headers:{
                     "Content-Type":"application/json"
@@ -64,8 +64,12 @@ async function postAddress(data){
     }
 }
 
-function Addresses(addresses){// this method create addreses list tables 
-    addresses.foreach(i =>{
+
+/****************************************************************************************************** This is a Normal  JS  *********************************************************************/
+
+function Addresses(addresses){ // this method create addreses list tables 
+    //console.log(addresses);
+    addresses.forEach(i=>{
 
         let div_but= document.createElement("div");
 
@@ -73,16 +77,38 @@ function Addresses(addresses){// this method create addreses list tables
         button.innerText= i.addressType;
 
         let dots= document.createElement("button");
-        dots.innerText= `<i class="fa-solid fa-ellipsis-vertical"></i>`;
+        dots.innerHTML= `<i class="fa-solid fa-ellipsis-vertical"></i>`;
+        dots.style.fontSize= "16px";
 
-        div_but.append(button, dots);
+    //**********************************************this section will help to edit or delete addresses for front end side*************************************************************************\\
+
+        let popDots= document.createElement("div");
+        popDots.setAttribute("class", "addressSetting");
+
+        let popDeleteEdit= document.createElement("div");
+        popDeleteEdit.setAttribute("class", "addressSettingDrop");
+
+        popDots.append(dots, popDeleteEdit);
+
+        let editPop= document.createElement("p");
+        editPop.innerText= "Edit";
+        let deletePop= document.createElement("p");
+        deletePop.innerText= "Delete";
+
+        popDeleteEdit.append(editPop,deletePop);
+
+
+
+    //888888888888888888888888888888888888888888888888    END THis Section    888888888888888888888888888888888888888888888888888888888888888888\\
+        div_but.append(button, popDots);
         div_but.setAttribute("class", "diff_but_dot");
 
         let ntag= document.createElement("p");
-        ntag.innerText= addresses.name +"           "+ addresses.mobileNumber;
+        ntag.innerText= i.aname +"        "+ i.aphoneNum;
+        ntag.style.fontWeight="600";
 
         let dtag= document.createElement("p");
-        dtag.innerText= addresses.streetAddress +" "+ addresses.locality +" "+ addresses.city +" "+ addresses.state +" - "+addresses.pinCode;
+        dtag.innerText= i.streetAddress +" "+ i.locality +" "+ i.city +" "+ i.state +" - "+i.pinCode;
         
         //let appendDiv= document.getElementById("manage_Addresses");
         addresslist_append.append(div_but, ntag, dtag) ;
@@ -91,8 +117,11 @@ function Addresses(addresses){// this method create addreses list tables
 
 };
 function ManageDivAddressOpen(){
-    let md= document.querySelector(".manage_Address_div");
-    md.style.display="none"
+    let md= document.querySelectorAll(".manage_Address_div");
+    for(let i=0; i<md.length; i++){
+        md[i].style.display="none"
+    }
+   
     let fd= document.querySelector("#addressFrom")
     fd.style.display="block";
     
@@ -102,4 +131,5 @@ function ManageDivAddressCancel(){
     document.querySelector("#addressFrom").style.display="none";
 
 }
+Addresses(addressListData);
 
